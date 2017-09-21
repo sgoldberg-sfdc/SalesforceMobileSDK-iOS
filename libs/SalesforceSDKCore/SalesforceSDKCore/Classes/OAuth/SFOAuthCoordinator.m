350024fac3efcf3fdaa5774852a37ece95421883
Bharath Hariharan <bhariharan@salesforce.com>
2017-09-21 13:28:03 -0700

    Updating ivar

--- libs/SalesforceSDKCore/SalesforceSDKCore/Classes/OAuth/SFOAuthCoordinator.m
+++ libs/SalesforceSDKCore/SalesforceSDKCore/Classes/OAuth/SFOAuthCoordinator.m
@@ -282,8 +282,7 @@ static NSString * const kSFAppFeatureSafariBrowserForLogin   = @"BW";
 }
 
 - (void)stopAuthentication {
-    [self.view stopLoading];
-    
+    [_view stopLoading];
     [self.session invalidateAndCancel];
     _session = nil;
     
